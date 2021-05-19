f147ed83cedd8146729928468dea1e1648d0d7af
WayneWang86 <wyf9686@gmail.com>
2021-05-19 09:22:21 +0800

    minor changes

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/database';
 
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBuaezDzBSMl-GnTni1Dmxg6TCDW4VZcYc",
-  authDomain: "travelsaver-a9449.firebaseapp.com",
-  projectId: "travelsaver-a9449",
-  storageBucket: "travelsaver-a9449.appspot.com",
-  messagingSenderId: "139452715123",
-  appId: "1:139452715123:web:b9c0e62a4e1afa27fe9a06"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBuaezDzBSMl-GnTni1Dmxg6TCDW4VZcYc",
+//   authDomain: "travelsaver-a9449.firebaseapp.com",
+//   projectId: "travelsaver-a9449",
+//   storageBucket: "travelsaver-a9449.appspot.com",
+//   messagingSenderId: "139452715123",
+//   appId: "1:139452715123:web:b9c0e62a4e1afa27fe9a06"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   // <React.StrictMode>
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ export class Home extends Component {
     render() {
         return(
             <div className="home">
-                <h1>Wanna go back to China? <br></br> We are here to help.</h1>
+                <h1>Wanna return back to China? <br></br> We are here to help.</h1>
                 <div className="query">
                     <label for="from">From Where:</label>
                     <input type="text" id="from" name="from"></input>
@@ -14,16 +14,10 @@ export class Home extends Component {
                     <label for="to">Where to in China</label>
                     <input type="text" id="to" name="to"></input>
 
-                    <label for="ticket">Ticket Number:</label>
-                    <input type="text" id="ticket" name="ticket"></input>
-
                     <br></br>
 
                     <label for="datet">Date:</label>
                     <input type="text" id="date" name="date"></input>
-
-                    <label for="passenger">Number of Passenger:</label>
-                    <input type="text" id="passenger" name="passenger"></input>
 
 
                     <a href="/timeline"><input type="submit" value="Submit"></input></a>
